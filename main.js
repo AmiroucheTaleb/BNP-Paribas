ee974e551f58fbe2f028c0aeb9f055b9feaf7f7d
AmiroucheTaleb <amirouche.tlb@gmail.com>
2023-01-10 09:46:14 +0100

    navbar maps

---
+++ main.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+var map = L.map('map').setView([36.716103, 3.204905], 8);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([36.716103, 3.204905]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").closePopup();
